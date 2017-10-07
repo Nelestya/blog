d6c1c2147bf76d4c9223d9d29e9af5159a42902e
Nelestya <tristand.pro@gmail.com>
2017-10-07 18:22:10 +0200

    add model post and the administration for this

--- admin.py
+++ admin.py
@@ -1,3 +1,51 @@
+import csv
+import datetime
+from django.core import serializers
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# ACTIONS
+def export_as_json(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type="application/json")
+    response['Content-Disposition'] = 'attachment; filename={}.json'.format(opts.verbose_name)
+    serializers.serialize("json", queryset, stream=response)
+    return response
+export_as_json.short_description = 'Export to JSON'
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # write the header row
+    writer.writerow([field.verbose_name for field in fields])
+    #write data row
+    for obj in queryset:
+        datarow = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            datarow.append(value)
+        writer.writerow(datarow)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'activate']
+    list_filter = ['status', 'created', 'updated', 'author', 'activate']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author',]
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    actions = [export_to_csv, export_as_json]
+
+admin.site.register(Post, PostAdmin)
--- models.py
+++ models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICE = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
+    activate = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
